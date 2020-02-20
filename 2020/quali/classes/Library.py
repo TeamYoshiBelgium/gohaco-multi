2fc51f7025ec897cc81880c9e618d0885aec099f
Ivan <ivan@lemon.be>
2020-02-20 18:56:28 +0100

    Library

---
+++ 2020/quali/classes/Library.py
@@ -0,0 +1,11 @@
+class Library:
+    CNTR = 0
+
+    def __init__(self, O, books, signup, rate):
+        self.O = O
+
+        self.books = books
+        self.rate = rate
+        self.signup = signup
+
+

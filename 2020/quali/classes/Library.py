ebdec2c85ae9fe0835a5a638e8ec1b649bb5b8bd
Stijn Goethals <stijn@tealpartners.com>
2020-02-20 19:14:10 +0100

    wip

--- 2020/quali/classes/Library.py
+++ 2020/quali/classes/Library.py
@@ -1,11 +1,12 @@
 class Library:
     CNTR = 0
 
-    def __init__(self, O, books, signup, rate):
+    def __init__(self, O, books_count, signup, rate, books):
         self.O = O
 
-        self.books = books
+        self.books_count = books_count
         self.rate = rate
         self.signup = signup
+        self.books = books
 
 
--- 2020/quali/classes/Loader.py
+++ 2020/quali/classes/Loader.py
@@ -1,17 +1,20 @@
 from .Optimizer import Optimizer
-
+from .Library import Library
 
 class Loader:
     def __init__(self, filename):
         self.filename = filename
         with open(filename) as file:
-           # self.O = Optimizer()
+            self.O = 1
+            # self.O = Optimizer()
 
             self.readHeaderLine(file)
             self.readBooks(file)
             self.readLibraries(file)
 
-            self.O.optimize()
+            for library in self.libraries:
+                print(len(library.books))
+#            self.O.optimize()
 
     def readHeaderLine(self, file):
         row = file.readline().split(" ")
@@ -46,7 +49,7 @@ class Loader:
                 books.append(int(book))
             id += 1
 
-            # library = Library(books_count, signup_time, books_day, books)
-            # self.libraries.append(library)
+            library = Library(self.O, books_count, signup_time, books_day, books)
+            self.libraries.append(library)
 
 

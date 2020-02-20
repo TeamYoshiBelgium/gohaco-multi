18a2f38dcedcd7e9f5633aa06f695c9e5f0db4d4
Ivan <ivanjdv@gmail.com>
2020-02-20 20:01:35 +0100

    Writer + score

--- 2020/quali/classes/Writer.py
+++ 2020/quali/classes/Writer.py
@@ -7,20 +7,20 @@ class Writer:
     def write(self):
         score = 0
         actions = []
-        for order in self.L.O.orders:
-            if (order.finished):
-                score += math.ceil((self.L.turns - order.finishTime)/self.L.turns * 100)
-
-        for drone in self.L.O.drones:
-            actions += drone.actions
+        for library in self.L.O.used_libraries:
+            for book in library:
+                score += book.score
 
         filename = self.L.filename.replace(".in", "." + str(score) + ".out").replace("in/", "out/")
 
         with open(filename, 'w+') as file:
-            file.write(str(len(actions)) + '\n')
-
-            for action in actions:
-                file.write(action.toString() + '\n')
+            file.write(str(len(self.L.O.used_libraries)))
+            file.write("\n")
+            for library in self.L.O.used_libraries:
+                file.write(str(library.id) + " " + str(len(library.scanned_books)))
+                file.write("\n")
+                for book in library.scanned_books:
+                    file.write(str(book.id))
 
         print(self.L.filename, "=>", filename)
 

01dac0879d75d5eee5e9ed5659e57f5fe60368f7
Stijn Goethals <stijn_goethals@hotmail.com>
2021-01-31 13:32:58 +0100

    init files

--- 2020/final/classes/Loader.py
+++ 2020/final/classes/Loader.py
@@ -1,16 +1,17 @@
 from .Optimizer import Optimizer
+from .MountPoint import MountPoint
 
 
 class Loader:
     def __init__(self, filename, heuristic_signup, heuristic_wasted): #, heuristic_useless, heuristic_signup, heuristic_bookcount, heuristic_realdays, trim):
-        self.books = []
+        self.map = []
         self.filename = filename
         with open(filename) as file:
             self.O = Optimizer(heuristic_signup, heuristic_wasted)#heuristic_useless, heuristic_signup, heuristic_bookcount, heuristic_realdays, trim)
 
-            self.readHeaderLine(file)
-            self.readBooks(file)
-            self.readLibraries(file)
+            self.read_header_line(file)
+            self.read_mount_points(file)
+            self.read_tasks(file)
 
             # for library in self.libraries:
             #     print(len(library.books))
@@ -25,46 +26,35 @@ class Loader:
             self.O.max = self.days
         pass
 
-    def readHeaderLine(self, file):
-        # row = file.readline().split(" ")
-        #
-        # self.books_length = int(row[0])
-        # self.librariees = int(row[1])
-        # self.days = int(row[2])
+    def read_header_line(self, file):
+        row = file.readline().split(" ")
+
+        self.maps_width = int(row[0])
+        self.maps_high = int(row[1])
+        self.arms_count = int(row[2])
+        self.mount_points_count = int(row[3])
+        self._tasks_count = int(row[4])
+        self._steps_count = int(row[5])
         pass
 
-    def readBooks(self, file):
-        # row = file.readline().split(" ")
-        # self.book_scores = []
-        # book_id = 0
-        # for book_score_str in row:
-        #     book_id += 1
-        #     book_score = int(book_score_str)
-        #     self.book_scores.append(book_score)
-        #     book = Book(self.O, book_id, book_score)
-        #     self.books.append(book)
+    def read_mount_points(self, file):
+        self.mount_points = []
+        for id in len(self.mount_points_count):
+            row = file.readline().split(" ")
+            x = int(row[0])
+            y = int(row[1])
+            mount_point = MountPoint(self.O, id, x, y)
+            self.mount_points.append(mount_point)
 
         pass
 
-    def readLibraries(self, file):
-        # self.libraries = []
-        # id = 0
-        #
-        # for id in range(self.librariees):
-        #     row = file.readline()
-        #     splitted = row.split(" ")
-        #     books_count = int(splitted[0])
-        #     signup_time = int(splitted[1])
-        #     books_day = int(splitted[2])
-        #
-        #     next_row = file.readline().split(" ")
-        #     books = []
-        #     library = Library(self.O, id, books_count, signup_time, books_day, books)
-        #     for book_id in next_row:
-        #         book = self.books[int(book_id)]
-        #         books.append(book)
-        #         book.addLibrary(library)
-        #     id += 1
-        #
-        #     self.libraries.append(library)
+    def read_tasks(self, file):
+        self.tasks = []
+        for id in len(self.mount_points_count):
+            row = file.readline().split(" ")
+            x = int(row[0])
+            y = int(row[1])
+            mount_point = MountPoint(self.O, id, x, y)
+            self.mount_points.append(mount_point)
+
         pass
---
+++ 2020/final/classes/Task.py
@@ -0,0 +1,5 @@
+class Tasks:
+    def __init__(self, optimizer, x, y):
+        self.O = optimizer
+        self.x = x
+        self.y = y

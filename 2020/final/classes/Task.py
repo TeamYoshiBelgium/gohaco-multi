e3ffb37840b4b76b62a7541556b4ed70fb107006
Stijn Goethals <stijn_goethals@hotmail.com>
2021-01-31 14:12:35 +0100

    wip

--- 2020/final/classes/Task.py
+++ 2020/final/classes/Task.py
@@ -1,4 +1,6 @@
 from . import Optimizer
+from .Arm import Arm
+import math
 
 class Task:
     CNTR = 0
@@ -23,3 +25,22 @@ class Task:
 
     def __str__(self):
         return "TASK%03s(%-03s/%03s)" % (self.No, self.score, self.points_count)
+
+    def get_score_per_moves(self, arm: Arm):
+        current_loc_x = arm.x
+        current_loc_y = arm.y
+        steps_needed = 0
+
+        for point in self.points:
+            distance_x = math.abs(current_loc_x - point.x)
+            distance_y = math.abs(current_loc_y - point.y)
+
+            steps_needed += distance_x + distance_y
+
+            current_loc_x = point.x
+            current_loc_y = point.y
+
+        if steps_needed == 0:
+            return self.score
+
+        return self.score / steps_needed

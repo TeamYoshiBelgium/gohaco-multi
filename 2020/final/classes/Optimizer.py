81a125e6f514ece0a9ce4903eb97c33b1b33af7c
JohannesB <johannes@berge.dev>
2021-01-31 14:57:40 +0100

    Fix optimizer

--- 2020/final/classes/Optimizer.py
+++ 2020/final/classes/Optimizer.py
@@ -36,27 +36,25 @@ class Optimizer:
         for mp in self.mountpoints:
             print(mp)
 
+    def find_best_tuple(self, list):
+        best = None
+        best_score = -99999999
+        for el in list:
+            if el[0] > best_score:
+                best = el[1]
+                best_score = el[0]
+
+
+        return best
+
     def find_best_mp(self, pool):
-
         mount_score_tuples = pool.map(self.parallelCalculation, self.mountpoints)
-
-        # for mp in mount_score_tuples:
-        #     print(mp)
-
-        self.mountpoints = map(
-            lambda tup: tup[1],
-              list(sorted(mount_score_tuples, reverse=True, key=lambda mp: mp[0]))
-        )
-
-        for mp in self.mountpoints:
-            print(mp)
+        return self.find_best_tuple(mount_score_tuples)
 
     def optimize(self):
-        # self.preprocess()
-
-
         with Pool(THREADS) as p:
             best_mountpoint = self.find_best_mp(p)
+
             i = 0
 
             while best_mountpoint is not None and i < len(self.arms):
@@ -68,11 +66,6 @@ class Optimizer:
 
         # for mp in mount_score_tuples:
         #     print(mp)
-
-        self.mountpoints = map(
-            lambda tup: tup[1],
-              list(sorted(mount_score_tuples, reverse=True, key=lambda mp: mp[0]))
-        )
 
         for mp in self.mountpoints:
             print(mp)

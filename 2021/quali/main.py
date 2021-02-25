9d63ee57a74c2d3c6434b91d70e67b66297a8273
JohannesB <johannes@berge.dev>
2021-02-25 19:38:24 +0100

    Add heuristics

--- 2021/quali/main.py
+++ 2021/quali/main.py
@@ -7,14 +7,14 @@ from classes.Settings import Settings
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] != "-a":
-        execute_file(sys.argv[1])
+        execute_file(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        execute_file("in/a.txt")
-        # execute_file("in/b.txt")
-        # execute_file("in/c.txt")
-        # execute_file("in/d.txt")
-        # execute_file("in/e.txt")
-        # execute_file("in/f.txt")
+        execute_file("in/a.txt", 0.05, 0.8)
+        # execute_file("in/b.txt", 0.05, 0.8)
+        # execute_file("in/c.txt", 0.05, 0.8)
+        # execute_file("in/d.txt", 0.05, 0.8)
+        # execute_file("in/e.txt", 0.05, 0.8)
+        # execute_file("in/f.txt", 0.05, 0.8)
 
 
 def execute_file(file_name, swap_vs_increment_heuristic, increment_decrement_heuristic):

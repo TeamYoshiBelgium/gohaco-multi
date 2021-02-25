9dfbc8359ac1f437c2501d90053f9a57b937cfdb
JohannesB <johannes@berge.dev>
2021-02-25 19:15:23 +0100

    Temp

--- 2021/quali/classes/Intersection.py
+++ 2021/quali/classes/Intersection.py
@@ -1,7 +1,18 @@
+
+
 class Intersection:
     def __init__(self, O, id):
         self.O = O
         self.id = id
+
+        self.trafficLightStreetTuples = []
+        self.carsThatPassThrough = []
+
+        for street in self.streets:
+            self.trafficLightStreetTuples.append((1, street))
+
+    def addCar(self, car):
+        self.cars.append(car)
 
     def __str__(self):
         return 'IS%i(%s)' % (self.id)

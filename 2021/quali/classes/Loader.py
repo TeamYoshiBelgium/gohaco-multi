4279c0ece84fad0ed5126b1ccb89df380ace5efa
Stijn Goethals <stijn.goethals@outlook.be>
2021-02-25 20:32:01 +0100

    wip

--- 2021/quali/classes/Loader.py
+++ 2021/quali/classes/Loader.py
@@ -17,11 +17,11 @@ class Loader:
             intersections = dict()
             for street in self.O.streets:
                 if street.start not in intersections:
-                    intersections[street.start] = [street.name]
+                    intersections[street.start] = [street]
                 else:
                     intersections[street.start].append(street)
                 if street.end not in intersections:
-                    intersections[street.end] = [street.name]
+                    intersections[street.end] = [street]
                 else:
                     intersections[street.end].append(street)
 

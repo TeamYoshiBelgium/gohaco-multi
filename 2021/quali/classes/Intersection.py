cdb2a9d44ba017139a398fe30db5f93620bec29a
JohannesB <johannes@berge.dev>
2021-02-25 21:14:30 +0100

    currentstreet green

--- 2021/quali/classes/Intersection.py
+++ 2021/quali/classes/Intersection.py
@@ -73,7 +73,7 @@ class Intersection:
             if car.finished is True:
                 continue
 
-            if car.nextStreet == greenStreet:
+            if car.currentStreet == greenStreet:
                 car.driveIntersection()
                 self.currentCars.remove(car)
                 return car

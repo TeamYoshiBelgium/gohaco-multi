916e718adc6de29678758366c6c22a8a6ae51460
JohannesB <johannes@berge.dev>
2021-02-25 15:41:46 +0100

    Update main.py

--- 2021/quali/main.py
+++ 2021/quali/main.py
@@ -6,19 +6,25 @@ from classes.Settings import Settings
 
 
 def main():
+    if len(sys.argv) > 1 and sys.argv[1] != "-a":
+        execute_file(sys.argv[1])
+    else:
+        execute_file("in/a_example.txt")
+        # execute_file("in/b_single_arm.txt")
+        # execute_file("in/d_tight_schedule.txt")
+        # execute_file("in/e_dense_workspace.txt")
+        # execute_file("in/f_decentralized.txt")
+
+
+def execute_file(file_name):
     S = Settings()
 
-    if len(sys.argv) != 2:
-        print("No argument provided!")
-        exit()
-
-    L = Loader(sys.argv[1])
-    O = Optimizer(L)
-
+    L = Loader(file_name)
     print("Script v%s started [%s]" % (S.version, L.filename))
-    O.optimize()
 
-    W = Writer(L, O)
+    L.O.optimize()
+
+    W = Writer(L, L.O)
     W.write()
 
 if __name__ == '__main__':

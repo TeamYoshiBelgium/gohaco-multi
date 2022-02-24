bff8a58324d34317aeddff1808b9b4cd1f76afe4
JohannesB <git@berge.dev>
2022-02-24 18:50:25 +0100

    Progress

--- 2022/quali/main.py
+++ 2022/quali/main.py
@@ -10,22 +10,22 @@ def main():
     if len(sys.argv) > 1 and sys.argv[1] != "-a":
         execute_file(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        execute_file("in/a.txt")
+        execute_file("in/a_an_example.in.txt")
         
         # print("="*80,end=os.linesep)
-        # execute_file("in/b.txt")
+        # execute_file("in/b_better_start_small.in.txt")
         
         # print("="*80,end=os.linesep)
-        # execute_file("in/c.txt")
+        # execute_file("in/c_collaboration.in.txt")
         
         # print("="*80,end=os.linesep)
-        # execute_file("in/d.txt")
+        # execute_file("in/d_dense_schedule.in.txt")
         
         # print("="*80,end=os.linesep)
-        # execute_file("in/e.txt")
+        # execute_file("in/e_exceptional_skills.in.txt")
         
         # print("="*80,end=os.linesep)
-        # execute_file("in/f.txt")
+        # execute_file("in/f_find_great_mentors.in.txt")
 
 
 def execute_file(file_name):

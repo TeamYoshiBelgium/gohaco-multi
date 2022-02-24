2535cb1d9a4b409d0d3ab3f97e4b85817ccfe53f
JohannesB <git@berge.dev>
2022-02-24 18:59:44 +0100

    Progress

--- 2022/quali/classes/Loader.py
+++ 2022/quali/classes/Loader.py
@@ -2,6 +2,8 @@ import os
 
 from .Engine import Engine
 from .Settings import Settings
+from .Person import Person
+from .Project import Project
 
 
 def read_int_array(file):
@@ -31,10 +33,43 @@ class Loader:
             # Usually first line is a parameter line
             self.params = read_int_array(file)
 
+            self.personCount = params[0]
+            self.projectCount = params[1]
+
+            self.persons = []
+            self.read_persons(file)
+
+            self.projects = []
+            self.read_projects(file)
+
             self.print_header()
 
             # read rest of file, read_array, read_int_array and stuff
 
+    def read_persons(self, file):
+
+        for i in range(self.personCount):
+            line = read_array(file)
+            name = line[0]
+            skillCount = int(line[1])
+
+            skills = {}
+            for j in range(skillCount):
+                line2 = read_array(file)
+                skillName = line2[0]
+                level = int(line2[1])
+
+                if skillName in skills:
+                    raise "Duplicate skill"
+
+                skills[skillName] = level
+
+            person = Person(name, skills)
+            self.persons.append(person)
+
+
+    def read_projects(self, file):
+        pass
 
     def get_heuristic(self):
         realFile = get_filename_no_extension(self.filename)

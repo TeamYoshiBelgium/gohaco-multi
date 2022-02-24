4ac2a293fe0450650a5597221c579663518ba209
Stijn Goethals <stijn@tealpartners.com>
2022-02-24 18:59:03 +0100

    add classes

--- 2022/quali/classes/Person.py
+++ 2022/quali/classes/Person.py
@@ -1,4 +1,11 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, skills):
         self.name = name
-        self.skills = dict()
+        self.time = 0
+        self.skills = skills
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+    def __repr__(self):
+        return str(self)
--- 2022/quali/classes/Project.py
+++ 2022/quali/classes/Project.py
@@ -1,11 +1,14 @@
 class Project:
 
-    def __init__(self, name, duration, score, before):
+    def __init__(self, name, duration, score, before, roles):
         self.name = name
         self.duration = duration
         self.score = score
         self.before = before
-        self.roles = dict()
+        self.roles = roles
 
     def __str__(self):
-        return "%s" % self.name
+        return "%s: %s %s %s" % (self.name, self.duration, self.score, self.before)
+
+    def __repr__(self):
+        return str(self)
